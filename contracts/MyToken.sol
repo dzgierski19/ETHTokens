894d3e925c40508e48b145fdbb4d95829764765b
dzgierski19 <dzgierskigit@gmail.com>
2023-11-12 22:32:55 +0100

    minted tokens

--- contracts/MyToken.sol
+++ contracts/MyToken.sol
@@ -4,5 +4,7 @@ pragma solidity >=0.7.0 <0.9.0;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract MyToken is ERC20 {
-    constructor() ERC20("MyToken", "MTK") {}
+    constructor() ERC20("MyToken", "MTK") {
+        _mint(msg.sender, 10 * 10 ** decimals());
+    }
 }
