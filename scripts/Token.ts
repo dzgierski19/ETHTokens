85f9964e8eaa85809c5ec952dde6a12e1b3009b9
dzgierski19 <dzgierskigit@gmail.com>
2023-11-12 22:25:55 +0100

    added initialSupply

--- scripts/Token.ts
+++ scripts/Token.ts
@@ -7,6 +7,10 @@ async function main() {
   await tokenContract.waitForDeployment();
   const tokenContractAddress = await tokenContract.getAddress();
   console.log(`Contract deployed at ${tokenContractAddress}`);
+  const initialSupply = await tokenContract.totalSupply();
+  console.log(
+    `The initial supply of this token is ${initialSupply.toString()} decimals units`
+  );
 }
 
 main().catch((err) => {
