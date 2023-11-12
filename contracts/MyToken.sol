ca31e134fa3f0d303e732eac166ae3e632a79dfe
dzgierski19 <dzgierskigit@gmail.com>
2023-11-12 22:40:05 +0100

    implementing access control

--- contracts/MyToken.sol
+++ contracts/MyToken.sol
@@ -2,9 +2,18 @@
 pragma solidity >=0.7.0 <0.9.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
 
-contract MyToken is ERC20 {
+contract MyToken is ERC20, AccessControl {
+    bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
+
     constructor() ERC20("MyToken", "MTK") {
-        _mint(msg.sender, 10 * 10 ** decimals());
+        _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
+    }
+
+    function mint(address to, uint256 amount) public onlyRole(MINTER_ROLE) {
+        _mint(to, amount);
     }
 }
+
+// whenever i run this smart contract I will mint 10*10**
