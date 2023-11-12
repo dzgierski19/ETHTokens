bd4ed4f20e88b6f2056869812204a035620520b8
dzgierski19 <dzgierskigit@gmail.com>
2023-11-12 21:57:43 +0100

    first commit

---
+++ contracts/MyToken.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.20;
+
+contract myToken {
+    uint256 _totaSupply;
+
+    constructor(){}
+    funciton totalSupply() public view returns (uint256) {
+}
